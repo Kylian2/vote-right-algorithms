888a80a0b2c8948cb3b80cff1e0e783ee7a0f8cb
Kylian2 <kylian.richard@gmail.com>
2025-02-02 21:47:57 +0100

    feat: endpoints parameters

--- src/main/java/fr/voteright/algorithms/controllers/ControllerProposal.java
+++ src/main/java/fr/voteright/algorithms/controllers/ControllerProposal.java
@@ -3,14 +3,15 @@ package fr.voteright.algorithms.controllers;
 import fr.voteright.algorithms.AlgorithmsApplication;
 import fr.voteright.algorithms.utils.HttpTools;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerProposal {
 
     @GetMapping(value="/proposals", produces = "application/json")
-    public String getProposalsOf() throws Exception {
-        return HttpTools.get(AlgorithmsApplication.baseUrl+"/algo/communities/"+13+"/proposals/formatted?period=2025");
+    public String getProposalsOf(@RequestParam int community, @RequestParam int period) throws Exception {
+        return HttpTools.get(AlgorithmsApplication.baseUrl+"/algo/communities/"+community+"/proposals/formatted?period="+period);
     }
-    
+
 }
